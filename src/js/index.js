95451efb97cca64ba9bce049fe43755dc9cbe487
Arnar Gauti Ingason <granra@gmail.com>
2015-06-06 18:05:49 +0000

    refactor

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ var cmd = mpd.cmd;
 var CA  = require('coverart');
 var $   = require('jquery');
 
+var state;
+
 var ca = new CA();
 
 $(document).ready(function() {
@@ -12,20 +14,43 @@ $(document).ready(function() {
 	});
 
 	client.on('ready', function() {
-		client.sendCommand(cmd("currentsong", []), function(err, msg) {
-			if (msg) {
-				var mbid = /MUSICBRAINZ_ALBUMID: (.*)\n/i.exec(msg)[1];
-				console.log(mbid);
-				fetchCoverArt(mbid).done(function(img) {
-					$('.cover').css('background-image',
-					"url('" + img +"')");
-				}).fail(function(res) {
-					console.log('fail');
+		client.sendCommand(cmd("status", []), function(err, msg) {
+			state = /state: (\w+)\n/i.exec(msg)[1];
+
+			if (state !== 'stop') {
+				client.sendCommand(cmd("currentsong", []), function(err, msg) {
+					if (msg) {
+						var mbid = /MUSICBRAINZ_ALBUMID: (.*)\n/i.exec(msg)[1];
+						updateCoverArt(mbid);
+					}
 				});
 			}
 		});
 	});
+
+	client.on('system-player', function(name) {
+		client.sendCommand(cmd("currentsong", []), function(err, msg) {
+			if (msg) {
+				var mbid = /MUSICBRAINZ_ALBUMID: (.*)\n/i.exec(msg)[1];
+				updateCoverArt(mbid);
+			}
+		});
+	});
 });
+
+var togglePlay = function() {
+
+};
+
+var updateCoverArt = function(mbid) {
+	fetchCoverArt(mbid)
+	.done(function(img) {
+		$('.cover').css('background-image',
+		"url('" + img +"')");
+	}).fail(function(res) {
+		console.log('fail');
+	});
+};
 
 var fetchCoverArt = function(mbid) {
 	var defer = $.Deferred();
