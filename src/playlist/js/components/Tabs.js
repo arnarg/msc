cc2c1dacd193d47cd7ddc6c9aa1063bc2194164f
Arnar Gauti Ingason <granra@gmail.com>
2015-07-22 20:34:59 +0000

    Styling of tabs

--- src/playlist/js/components/Tabs.js
+++ src/playlist/js/components/Tabs.js
@@ -5,6 +5,12 @@ var Tabs = React.createClass({
 	render: function() {
 		return (
 			<div className='tabBar'>
+				<div className='tab active'>
+					<i className='fa fa-list'></i> Playlist
+				</div>
+				<div className='tab'>
+					<i className='fa fa-music'></i> Library
+				</div>
 			</div>
 		);
 	}
