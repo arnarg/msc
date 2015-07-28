92f7a8229fadf1c057f5dd1b505b43875bd97e39
Arnar Gauti Ingason <granra@gmail.com>
2015-07-28 23:17:48 +0000

    Updating gitignore

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="github-electron/github-electron-main.d.ts" />
-/// <reference path="github-electron/github-electron.d.ts" />
-/// <reference path="nconf/nconf.d.ts" />
-/// <reference path="node/node.d.ts" />
